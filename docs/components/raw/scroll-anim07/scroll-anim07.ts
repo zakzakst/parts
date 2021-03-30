476fd8ef7e8b154e8965257e6857ea56029064a5
zakzakst <zakzakst@users.noreply.github.com>
2021-03-30 16:48:55 +0000

    deploy: fc42248fa6aa4b259b179e21cc18b2a9badebacd

---
+++ docs/components/raw/scroll-anim07/scroll-anim07.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+import { gsap } from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+export const scrollAnim07 = () => {
+  // アニメーションさせる要素を指定
+  const els = document.getElementsByClassName('js-scroll-anim-07');
+  // アニメーションを設定
+  [...els].forEach(el => {
+    const imgAnim = new Anim07(<HTMLElement>el);
+    imgAnim.init();
+  });
+}
+
+class Anim07 {
+  el: HTMLElement;
+  constructor(el: HTMLElement) {
+    this.el = el;
+  }
+
+  /**
+   * 初期化
+   */
+  init(): void {
+    this.scrollHandler();
+  }
+
+  /**
+   * スクロール連動のイベント設定
+   */
+  scrollHandler(): void {
+    ScrollTrigger.create({
+      trigger: this.el,
+      start: "top 70%",
+      onEnter: self => {
+        // 「is-animated」クラスを付与
+        this.el.classList.add('is-animated');
+        self.kill();
+      }
+    });
+  }
+}
