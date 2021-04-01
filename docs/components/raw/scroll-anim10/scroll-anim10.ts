1f5f915354e0b87dbcf87ed9e8993c0c4b32bfe9
zakzakst <zakzakst@users.noreply.github.com>
2021-04-01 19:57:58 +0000

    deploy: 1efe3a447111e54bfb02f09672fbfcbbc64fbbef

---
+++ docs/components/raw/scroll-anim10/scroll-anim10.ts
@@ -0,0 +1,60 @@
+'use strict';
+
+import { gsap } from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+export const scrollAnim10 = () => {
+  // アニメーションさせる要素を指定
+  const els = document.getElementsByClassName('js-scroll-anim-10');
+  // アニメーションを設定
+  [...els].forEach(el => {
+    const imgAnim = new Anim10(<HTMLElement>el);
+    imgAnim.init();
+  });
+}
+
+class Anim10 {
+  el: HTMLElement;
+  constructor(el: HTMLElement) {
+    this.el = el;
+  }
+
+  /**
+   * 初期化
+   */
+  init(): void {
+    this.textInit();
+    this.scrollHandler();
+  }
+
+  /**
+   * テキストを初期化
+   */
+  textInit(): void {
+    // アニメーション用に分割
+    let markup = '';
+    const text = this.el.textContent;
+    text.split('').forEach(l => {
+      markup += `
+        <span>${l}</span>
+      `;
+    });
+    this.el.innerHTML = markup;
+  }
+
+  /**
+   * スクロール連動のイベント設定
+   */
+  scrollHandler(): void {
+    ScrollTrigger.create({
+      trigger: this.el,
+      start: "top 70%",
+      onEnter: self => {
+        // 「is-animated」クラスを付与
+        this.el.classList.add('is-animated');
+        self.kill();
+      }
+    });
+  }
+}
---
+++ src/components/scroll-anims/scroll-anim10/README.md
@@ -0,0 +1,3 @@
+## 参考
+
+<a href="https://ecopet.info/" target="_blank">https://ecopet.info/</a>
