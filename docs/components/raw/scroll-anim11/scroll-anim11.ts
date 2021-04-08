f0ec917101e3e284e4a04daec6810c997d834c5e
zakzakst <zakzakst@users.noreply.github.com>
2021-04-08 14:35:22 +0000

    deploy: 4fd6a23df3f0c3b312f056a683fec03147b830d0

---
+++ docs/components/raw/scroll-anim11/scroll-anim11.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+import { gsap } from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+export const scrollAnim11 = () => {
+  // アニメーションさせる要素を指定
+  const els = document.getElementsByClassName('js-scroll-anim-11');
+  // アニメーションを設定
+  [...els].forEach(el => {
+    const imgAnim = new Anim11(<HTMLElement>el);
+    imgAnim.init();
+  });
+}
+
+class Anim11 {
+  el: HTMLElement;
+  constructor(el: HTMLElement) {
+    this.el = el;
+  }
+
+  /**
+   * 初期化
+   */
+  init(): void {
+    this.scrollHandler();
+  }
+
+  /**
+   * スクロール連動のイベント設定
+   */
+  scrollHandler(): void {
+    ScrollTrigger.create({
+      trigger: this.el,
+      start: "top 70%",
+      onEnter: self => {
+        // 「is-animated」クラスを付与
+        this.el.classList.add('is-animated');
+        self.kill();
+      }
+    });
+  }
+}
---
+++ src/components/scroll-anims/scroll-anim11/README.md
@@ -0,0 +1,3 @@
+## 参考
+
+<a href="https://beartail.jp/" target="_blank">https://beartail.jp/</a>
