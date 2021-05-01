05a6e4a40636ea669e67c9038daae9e7419c97c8
zakzakst <zakzakst@users.noreply.github.com>
2021-05-01 17:10:01 +0000

    deploy: 7715315c5312fb67fac7517ec1c5f0497e918bc3

---
+++ src/work/scroll-anim.ts
@@ -0,0 +1,59 @@
+/**
+ * スクロールアニメーション
+ */
+ import { scrollAnim01 } from '../components/scroll-anims/1_20/scroll-anim01/scroll-anim01';
+ scrollAnim01();
+
+ import { scrollAnim02 } from '../components/scroll-anims/1_20/scroll-anim02/scroll-anim02';
+ scrollAnim02();
+
+ import { scrollAnim03 } from '../components/scroll-anims/1_20/scroll-anim03/scroll-anim03';
+ scrollAnim03();
+
+ import { scrollAnim04 } from '../components/scroll-anims/1_20/scroll-anim04/scroll-anim04';
+ scrollAnim04();
+
+ import { scrollAnim05 } from '../components/scroll-anims/1_20/scroll-anim05/scroll-anim05';
+ scrollAnim05();
+
+ import { scrollAnim06 } from '../components/scroll-anims/1_20/scroll-anim06/scroll-anim06';
+ scrollAnim06();
+
+ import { scrollAnim07 } from '../components/scroll-anims/1_20/scroll-anim07/scroll-anim07';
+ scrollAnim07();
+
+ import { scrollAnim08 } from '../components/scroll-anims/1_20/scroll-anim08/scroll-anim08';
+ scrollAnim08();
+
+ import { scrollAnim09 } from '../components/scroll-anims/1_20/scroll-anim09/scroll-anim09';
+ scrollAnim09();
+
+ import { scrollAnim10 } from '../components/scroll-anims/1_20/scroll-anim10/scroll-anim10';
+ scrollAnim10();
+
+ import { scrollAnim11 } from '../components/scroll-anims/1_20/scroll-anim11/scroll-anim11';
+ scrollAnim11();
+
+ import { scrollAnim12 } from '../components/scroll-anims/1_20/scroll-anim12/scroll-anim12';
+ scrollAnim12();
+
+ import { scrollAnim13 } from '../components/scroll-anims/1_20/scroll-anim13/scroll-anim13';
+ scrollAnim13();
+
+ import { scrollAnim15 } from '../components/scroll-anims/1_20/scroll-anim15/scroll-anim15';
+ scrollAnim15();
+
+ import { scrollAnim16 } from '../components/scroll-anims/1_20/scroll-anim16/scroll-anim16';
+ scrollAnim16();
+
+ import { scrollAnim17 } from '../components/scroll-anims/1_20/scroll-anim17/scroll-anim17';
+ scrollAnim17();
+
+ import { scrollAnim18 } from '../components/scroll-anims/1_20/scroll-anim18/scroll-anim18';
+ scrollAnim18();
+
+ import { scrollAnim19 } from '../components/scroll-anims/1_20/scroll-anim19/scroll-anim19';
+ scrollAnim19();
+
+ import { scrollAnim20 } from '../components/scroll-anims/1_20/scroll-anim20/scroll-anim20';
+ scrollAnim20();
