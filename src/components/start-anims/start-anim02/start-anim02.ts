cdd18aec6c6185a6bb241cb43826487e5820b3fc
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-05-04 23:25:47 +0900

    開始アニメーション02

--- src/components/start-anims/start-anim02/start-anim02.ts
+++ src/components/start-anims/start-anim02/start-anim02.ts
@@ -58,38 +58,3 @@ class StartAnim02 {
     };
   }
 }
-
-
-
-
-
-import $ from 'jquery';
-
-export function pageLoader() {
-  $(window).on('load', () => {
-    openAnim();
-  });
-}
-
-function openAnim() {
-  const tl = gsap.timeline({
-    onComplete() {
-      $('#js-page-loader').remove();
-      setTimeout(() => {
-        $('.hero__ttl, .hero__subttl').addClass('is-animated');
-      }, 400);
-    }
-  });
-  tl.to('.page-loader__content', {
-    duration: .3,
-    opacity: 0
-  })
-  .to('.page-loader__bg-item', {
-    delay: .7,
-    duration: .3,
-    width: 0,
-    stagger: {
-      each: .1
-    }
-  });
-}
