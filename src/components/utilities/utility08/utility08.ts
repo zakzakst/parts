c761b48361c7f40ca5a68233d7c7f2f077d1871e
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-11-20 21:13:19 +0900

    汎用パーツ08

--- src/components/utilities/utility08/utility08.ts
+++ src/components/utilities/utility08/utility08.ts
@@ -12,11 +12,13 @@ class Utility08 {
   el: HTMLElement;
   inputEl: HTMLInputElement;
   startEl: HTMLElement;
+  stopEl: HTMLElement;
   targetDate: Date;
   constructor() {
     this.el = document.getElementById('js-utility-08');
     this.inputEl = <HTMLInputElement>document.getElementById('js-utility-08-input');
     this.startEl = document.getElementById('js-utility-08-start');
+    this.stopEl = document.getElementById('js-utility-08-stop');
     this.targetDate = null;
   }
 
@@ -27,6 +29,7 @@ class Utility08 {
     if (!this.el) return;
     this.inputInit();
     this.onClickStart();
+    this.onClickStop();
   }
 
   /**
@@ -46,16 +49,51 @@ class Utility08 {
   /**
    * カウントダウン開始
    */
-  countDownStart(): void {
+  countDown(): void {
     const now = new Date();
-    if (this.targetDate.getTime() > now.getTime()) {
-      // 未来の日付が設定されている場合、カウントダウンを開始
-      console.log(this.targetDate);
-    } else {
-      // 未来の日付が設定されていない場合、処理を終了
-      alert('カウントダウンの目標日時は未来の時間を設定してください。');
+    // 現在日時と目標日時の秒数差分を計算
+    const diff = this.targetDate.getTime() - now.getTime();
+    // 目標日時を過ぎている場合、カウントダウン完了
+    if (diff <= 0) {
+      this.countDownFinish();
       return;
     }
+    // 差分から残り日時の数値を計算
+    const d = Math.floor(diff / (24 * 60 * 60 * 1000));
+    const h = Math.floor(diff % (24 * 60 * 60 * 1000) / (60 * 60 * 1000));
+    const m = Math.floor(diff % (60 * 60 * 1000) / (60 * 1000));
+    const s = Math.floor(diff % (60 * 1000) / 1000);
+    // カウントダウン文字列を設定
+    this.countDownSetText(d, h, m, s);
+    // 1秒後にカウントダウンを実行
+    setTimeout(() => {
+      this.countDown();
+    }, 1000);
+  }
+
+  /**
+   * カウントダウン文字列を設定
+   * @param day 残りの日
+   * @param hour 残りの時間
+   * @param minute 残りの分
+   * @param second 残りの秒
+   */
+  countDownSetText(day: number, hour: number, minute: number, second: number): void {
+    console.log(day, hour, minute, second);
+  }
+
+  /**
+   * カウントダウンを完了
+   */
+  countDownFinish(): void {
+    console.log('カウントダウン完了');
+  }
+
+  /**
+   * カウントダウンを中止
+   */
+  countDownStop(): void {
+    console.log('カウントダウン中止');
   }
 
   /**
@@ -66,7 +104,7 @@ class Utility08 {
       // 入力されている日時から目標日時を設定
       const inputVal = this.inputEl.value;
       const year = Number(inputVal.substring(0, 4));
-      const month = Number(inputVal.substring(5, 7));
+      const month = Number(inputVal.substring(5, 7)) - 1;
       const day = Number(inputVal.substring(8, 10));
       const hour = Number(inputVal.substring(14, 16));
       const minute = Number(inputVal.substring(17, 19));
@@ -77,15 +115,24 @@ class Utility08 {
         hour,
         minute
       );
-      // カウントダウン開始
-      this.countDownStart();
+      const now = new Date();
+      if (this.targetDate.getTime() > now.getTime()) {
+        // 未来の日付が設定されている場合、カウントダウンを開始
+        this.countDown();
+      } else {
+        // 未来の日付が設定されていない場合、処理を終了
+        alert('カウントダウンの目標日時は未来の時間を設定してください。');
+        return;
+      }
     });
   }
 
-  // カウントダウン先の日時を設定（現在時刻以降の時間のみ設定できるようにする）
-  // クッキーにカウントダウン先の日時を保存
-  // カウントダウンを開始
-  // カウントダウン先の日時と比較して残りの時間を計算
-  // カウントダウンの文字を表示
-  // カウントダウンをやめる
+  /**
+   * 中止ボタンクリック時のイベント設定
+   */
+  onClickStop(): void {
+    this.stopEl.addEventListener('click', () => {
+      this.countDownStop();
+    });
+  }
 }
