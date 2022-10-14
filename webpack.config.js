d7a539edecc7d940233b17105947cbadc53a2396
zakzakst <e1433104@gmail.com>
2022-10-14 19:03:05 +0900

    その他13

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: {
-    // 'script': './src/work/script.ts',
+    'script': './src/work/script.ts',
     // 'scroll-anim': './src/work/scroll-anim.ts',
-    'utilities': './src/work/utilities.ts',
+    // 'utilities': './src/work/utilities.ts',
   },
   resolve: {
     extensions: [
